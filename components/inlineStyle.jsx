b1ca22f0df0d2257ba350b8079976d97c1f611a9
sakurakotubaki <73347485+sakurakotubaki@users.noreply.github.com>
2021-09-05 16:15:05 +0900

    inlineStyleについて object形式でcssを書いていく

--- components/inlineStyle.jsx
+++ components/inlineStyle.jsx
@@ -1,8 +1,27 @@
 export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2x #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "#3ed84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
   return (
-    <div>
-      <p>- Inline Style -</p>
-      <button>FIGHT</button>
+    <div style={containerStyle}>
+      <p style={titleStyle}>- Inline Style -</p>
+      <button style={buttonStyle}>FIGHT</button>
     </div>
   );
 };
